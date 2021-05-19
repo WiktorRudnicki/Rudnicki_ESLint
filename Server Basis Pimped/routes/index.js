9b95091950398c12402653e56b45304daebdb18b
WiktorRudnicki <rudnicki.w01@htlwienwest.at>
2021-05-19 18:22:12 +0200

    route für GET

--- Server Basis Pimped/routes/index.js
+++ Server Basis Pimped/routes/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
-// const asyncHandler = require('express-async-handler');
-
+const asyncHandler = require('express-async-handler');
+const {getCock }= require('../functions/funk.js')
 const router = express.Router();
+
+router.get(
+    '/cocktails',
+    asyncHandler(async (req, res) => {
+        let res = await getCock();
+        res.status(resulst.status).send(result.data);
+    })
+);
 
 module.exports = router;
